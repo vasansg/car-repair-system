241773e78040b2979dc97f6f5535ce6cf8b13d5a
portfolio <vasanthavanan12@gmail.com>
2026-05-25 06:25:36 +0800

    add SSL option for Aiven MySQL connection

--- api/includes/config.php
+++ api/includes/config.php
@@ -49,6 +49,7 @@ try {
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ]);
 } catch (PDOException $e) {
     http_response_code(500);
